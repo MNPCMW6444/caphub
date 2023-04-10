8b963c24bff85a0fede725baf0763cc2368b13da
Michael Nusair‬‏ <30693526+MNPCMW6444@users.noreply.github.com>
2023-04-10 19:15:51 +0300

    awdwqd

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { UserContextProvider } from "./context/UserContext";
-import Lp from "./lp/LP";
+import App from "./App";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <Lp />
+      <App />
     </UserContextProvider>
   </React.StrictMode>
 );
